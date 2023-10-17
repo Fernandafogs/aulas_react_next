3120e4b490cdb954fc309a9b4a1170a451c9103f
Fernanda Fogaca <fernanda.ramos.br@gmail.com>
2023-10-17 14:34:37 +0100

    exemplo de utilizacao context com contador

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Principal</h1>
+      <Link href="/contexto/contador">
+        <span>Contador</span>
+      </Link>
     </div>
   );
 }
