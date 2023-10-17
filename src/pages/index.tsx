6abd7e34c7cb4040158c47d098b7027316ddc267
Fernanda Fogaca <fernanda.ramos.br@gmail.com>
2023-10-17 15:14:04 +0100

    lista de compras utilizando useContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,14 @@ export default function Home() {
   return (
     <div>
       <h1>Principal</h1>
-      <Link href="/contexto/contador">
-        <span>Contador</span>
-      </Link>
+      <div className="flex flex-col">
+        <Link href="/contexto/contador">
+          <span>Contador</span>
+        </Link>
+        <Link href="/listas/produtos">
+          <span>Produtos</span>
+        </Link>
+      </div>
     </div>
   );
 }
